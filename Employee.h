a0b1054bc90405569797601d3311cd76bd1124a8
deasn789 <60975757+deasn789@users.noreply.github.com>
2020-04-25 00:23:04 -0400

    Added comments for Kurdi and included string

--- Employee.h
+++ Employee.h
@@ -1,18 +1,22 @@
 #pragma once
-#include"Person.h"
+#include "Person.h"
+#include <string>
 
+// derrived class of Person
 class Employee : public Person
 {
 public:
+	// constructors
 	Employee();
 	Employee(std::string fname, std::string lname, std::string stAdress, int zip, std::string city, int dbirth, int mbirth, int ybirth, std::string ssn, char gen, double salary);
-
+	// accessor
 	double getSalary();
+	// setter/mutator
 	void setSalary(double newSal);
 
 
 protected:
-	double salary_;
+	double salary_; // every employee has a their own salary
 
 
 };
