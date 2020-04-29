b8f984e5c184b0c81f4e9c4ee5a4db96a96ac5ea
Nathan Deas <60975757+deasn789@users.noreply.github.com>
2020-04-29 03:12:51 -0400

    started working on nurse

---
+++ Nurse.h
@@ -0,0 +1,34 @@
+#include "Employee.h"
+#include <string>
+
+// derived class of Employee
+ class Nurse public: Employee
+{
+  public:
+    // constructors and destructor
+    Classname();
+    Classname(var, var);
+    Classname(const Classname& orig)
+    ~Classname();
+
+    // accessors:
+    int getVar();
+    int getVar();
+
+    // setters/modifiers:
+    void setVar(varType);
+    void setVar(varType);
+
+    // other functions:
+    void otherFunction01();
+    void otherFunction02();
+
+  Protected:
+    // static Variable:
+    static int nurseIDs_;   // Nurses have own static int for ID
+    // other Variables:
+    std::string specialty;
+    bool practitioner;
+    nbNurses_;
+    int idNurse_;
+};
---
+++ nurse.cpp
@@ -0,0 +1,52 @@
+#include "Nurse.h"
+
+// static variable
+int Nurse::nurseIDs_ = 0;
+
+// constructors and destructor
+Nurse::Nurse()
+{
+
+}
+Nurse::Nurse(var, var)
+{
+
+}
+Nurse::Nurse(const Classname& orig)
+{
+
+}
+Nurse::~Nurse()
+{
+
+}
+
+// accessors:
+int Nurse::getVar()
+{
+
+}
+int Nurse::getVar()
+{
+
+}
+
+// settors/modifiers:
+void Nurse::setVar(varType)
+{
+
+}
+void Nurse::setVar(varType)
+{
+
+}
+
+// other functions:
+void Nurse::otherFunction01()
+{
+
+}
+void Nurse::otherFunction02()
+{
+
+}
