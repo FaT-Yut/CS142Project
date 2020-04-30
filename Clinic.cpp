6e6b36d283026e586d35fb79df48310db3865a1d
Nathan Deas <60975757+deasn789@users.noreply.github.com>
2020-04-30 03:47:59 -0400

    Fixed some typos

--- Clinic.cpp
+++ Clinic.cpp
@@ -1,56 +1,56 @@
-#include "Clinc.h"
+#include "Clinic.h"
 
-Clinic::Clinic(): arNurses_(0), arDoctors_(0), nbNurses_(0), totalNBNurses_(0) {}
+Clinic::Clinic() : arNurses_(0), arDoctors_(0), nbNurses_(0), totalNBNurses_(0) {}
 
-Clinic::Clinic(Nurses** nurses, Doctor** doctors, int nbNurses, int totalNurses):
-arNurses_(nurses), arDoctors_(doctors), nbNurses_(nbNurses), totalNBNurses_(totalNurses)
+Clinic::Clinic(Nurse** nurses, Doctor** doctors, int nbNurses, int totalNurses) :
+    arNurses_(nurses), arDoctors_(doctors), nbNurses_(nbNurses), totalNBNurses_(totalNurses)
 {}
 
 void Clinic::addNurse(Nurse nurse)
 {
-  Nurse* ptr = &nurse;
-  // Places the nurse in the next avaiable spot and then moves the
-  // pointer to the next spot
-  arNurses_[nbNurses_++] = ptr;
+    Nurse* ptr = &nurse;
+    // Places the nurse in the next avaiable spot and then moves the
+    // pointer to the next spot
+    arNurses_[nbNurses_++] = ptr;
 }
 
 void Clinic::delNurse(Nurse nurse)
 {
     for (int i = 0; i < nbNurses_; i++)
     {
-      // Finds the nurse object in the array and
-      // deletes the object
-      if (arNurses_[i] == nurse)
-      {
-        delete arNurses_[i];
-
-        // Keeps size of the array, but moves each Nurse up
-        // in the array so that a new Nurse can be added
-        for (j = i; j < (nbNurses_ - 1); j++)
+        // Finds the nurse object in the array and
+        // deletes the object
+        if (arNurses_[i] == nurse)
         {
-          arNurses_[j] = arNurses_[j + 1];
+            delete arNurses_[i];
+
+            // Keeps size of the array, but moves each Nurse up
+            // in the array so that a new Nurse can be added
+            for (j = i; j < (nbNurses_ - 1); j++)
+            {
+                arNurses_[j] = arNurses_[j + 1];
+            }
         }
-      }
     }
 }
 
 bool Clinc::checkNurse(int hour, int day, int month, int year, int idNurse)
 {
-  bool booked = false;
-  int nbDocs = 2;
+    bool booked = false;
+    int nbDocs = 2;
 
-  for (int i = 0; i < nbDocs; i++)
-  {
-    if (arDoctors_[i].getAppointment().day == day &&
-    arDoctors_[i].getAppointment().month == month &&
-    arDoctors_[i].getAppointment().year == year &&
-    arDoctors_[i].getAppointment().hour == hour &&
-    arDoctors_[i].getAppointment().nurseID == idNurse)
+    for (int i = 0; i < nbDocs; i++)
     {
-      // Check nurse avaiablility
-      booked = true;
+        if (arDoctors_[i].getAppointment().day == day &&
+            arDoctors_[i].getAppointment().month == month &&
+            arDoctors_[i].getAppointment().year == year &&
+            arDoctors_[i].getAppointment().hour == hour &&
+            arDoctors_[i].getAppointment().nurseID == idNurse)
+        {
+            // Check nurse avaiablility
+            booked = true;
+        }
     }
-  }
 
-  return booked;
+    return booked;
 }
