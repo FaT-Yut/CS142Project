7c656ef85572d90c6e043c5019ce197b2dc7980c
Joseph Hurowitz <hurowitz_jb@lynchburg.edu>
2020-04-29 22:10:15 -0400

    Clinic hopefully done

---
+++ Clinic.h
@@ -0,0 +1,19 @@
+#include "Doctor.h"
+#include "Nurse.h"
+
+class Clinic
+{
+public:
+  Clinic();
+  Clinic(Nurses**, Doctor**, int, int);
+
+  void addNurse(Nurse nurse);
+  void delNurse(Nurse);
+  bool checkNurse(int hour, int day, int month, int year, int idNurse);
+
+private:
+  int nursesInAR_;
+  int totalNBNurses_;
+  Nurse** arNurses_;
+  Doctor** arDoctors_;
+};
