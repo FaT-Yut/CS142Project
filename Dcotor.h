b83c92623481d9e4f7c7bc60dc82aa518e8ab9d0
Nathan Deas <60975757+deasn789@users.noreply.github.com>
2020-04-30 03:45:10 -0400

    Included Patient

--- Dcotor.h
+++ Dcotor.h
@@ -1,22 +1,23 @@
 #pragma once
 #include "Employee.h"
+#include "Patient.h"
 #include "Appointment.h"
 
 class Doctor : public Employee
 {
 public:
-  Doctor();
-  Doctor(std::string first, std::string last, std::string address, int zip, std::string city, int day, int month, int year, int ssn, char gend, double salary, int docID, std::string specialty, std::string degree, std::string medSchool, bool certified, Patient* pat, Appointment* app );
+    Doctor();
+    Doctor(std::string first, std::string last, std::string address, int zip, std::string city, int day, int month, int year, int ssn, char gend, double salary, int docID, std::string specialty, std::string degree, std::string medSchool, bool certified, Patient* pat, Appointment* app);
 
 
 private:
-  int docID_;
-  std::string specialty_;
-  std::string degree_;
-  std::string medSchool_;
-  bool certified_;
-  Patient* patArray_;
-  Appointment* appArray_;
-  static int counter;
+    int docID_;
+    std::string specialty_;
+    std::string degree_;
+    std::string medSchool_;
+    bool certified_;
+    Patient* patArray_;
+    Appointment* appArray_;
+    static int counter;
 
 };
