e1a7233ea1ec79c9e69ec2f866b37ddb8841e1c6
Keegan Pursley <thespiskeegan@gmail.com>
2020-04-30 12:42:29 -0400

    Doctor Class update

--- Doctor.cpp
+++ Doctor.cpp
@@ -15,5 +15,58 @@ Doctor::Doctor(std::string fName, std::string lName, std::string stAdress,
     medSchool_(medSchool), certified_(certified), patArray_(pat), appArray_(app)
     {}
 {
-  int docID_ = counter++; // ??
+  docID_ = counter++; // ??
+}
+
+void Doctor::addPatient(Patient* pat)
+{
+    nbPatients_++;
+    Patient** temp = new Patient * [nbPatients_];
+    for (int i = 0; i < (nbPatients_ - 1); i++)
+    {
+        temp[i] = patArray_[i];
+    }
+    temp[(nbPatients_ - 1)] = pat;
+
+    delete[] patArray_;
+
+    patArray_ = new Patient * [nbPatients_];
+    for (int i = 0; i < nbPatients_; i++)
+    {
+        patArray_[i] = temp[i];
+    }
+
+    delete[] temp;
+}
+
+
+void Doctor::addAppointment(int idPatient, int hour, int day, int month, int year, int idNurse)
+{
+    nbApp_++; // increment the total number of vehicles by one since adding a new vehicle
+    Appointment** temp = new Appointment * [nbApp_]; // create a temp array of pointers that will represent the value that we are changing the array to
+    for (int i = 0; i < (nbApp_ - 1); i++) // put in old values into new array up to the last spot
+    {
+        temp[i] = appArray_[i];
+    }
+    Appointment* tempApp;
+    tempApp->hour_ = hour;
+    tempApp->day_ = day;
+    tempApp->month_ = month;
+    tempApp->year_ = year;
+    tempApp->patientID_ = idPatient;
+    tempApp->nurseID_ = idNurse;
+
+    temp[(nbApp_ - 1)] = tempApp; // fill the last spot with the new vehicle being added
+
+    delete[] appArray_; // deallocate the old arrary held within Vehicle** availableVehicles_
+
+    appArray_ = new Appointment * [nbApp_]; // allocate the new memory needed to hold the slightly bigger array
+    for (int i = 0; i < nbApp_; i++) // fill new memory allocated with the correct value stored in temp
+    {
+        appArray_[i] = temp[i];
+    }
+
+    delete[] temp; // deallocate temp
+
+
 }
--- Doctor.h
+++ Doctor.h
@@ -10,7 +10,12 @@ public:
     Doctor(std::string, std::string, std::string, int, std::string, int, int,
       int, int, char, double, int, std::string, std::string, std::string, bool,
        Patient*, Appointment*);
-      
+
+  void addPatient(Patient* pat);
+
+  void addAppointment(int idPatient, int hour, int day, int month, int year, int idNurse);
+
+
 private:
     int docID_;
     std::string specialty_;
@@ -20,4 +25,6 @@ private:
     Patient* patArray_;
     Appointment* appArray_;
     static int counter;
+    int nbApp_;
+    int nbPatients_;
 };
