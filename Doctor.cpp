02b5850e8dd9fd7dc3f2ddc546193972b286c4a4
Keegan Pursley <thespiskeegan@gmail.com>
2020-04-30 17:46:44 -0400

    Doctor

--- Doctor.cpp
+++ Doctor.cpp
@@ -151,11 +151,15 @@ void Doctor::showAppointments()
 {
     for (int i = 0; i < nbApp_; i++)
     {
-        if(appArray_[i]->year_ == 0)
+        if(appArray_[i]->year_ != 0)
         {
         std::cout << "Patient with ID Number " << appArray_[i]->patientID_ << " Has an appointment at " << appArray_[i]->hour_ << " O'clock "
             << " In the month of " << appArray_[i]->month_ << " on the day of " << appArray_[i]->day_ << " in the year " << appArray_[i]->year_
-            << " with the Nurse with the ID number of " << appArray_[i]->nurseID_;
+            << " with the Nurse with the ID number of " << appArray_[i]->nurseID_ << std::endl;
+        }
+        else
+        {
+          std::cout << "No Appointments scheduled" << std::endl;
         }
     }
 }
