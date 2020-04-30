c5b25f8a457655c9f1152f33aa8826adf51c4d77
Keegan Pursley <thespiskeegan@gmail.com>
2020-04-30 12:03:55 -0400

    deleteApp needs work

--- Doctor.cpp
+++ Doctor.cpp
@@ -20,7 +20,7 @@ Doctor::Doctor()
   std::string medSchool_ = "N/A";
 }
 
-Doctor::Doctor(std::string first, std::string last, std::string address, int zip, std::string city, int day, int month, int year, int ssn, char gend, double salary, std::string specialty, std::string degree, std::string medSchool, bool certified, Patient* pat, Appointment* app )
+Doctor::Doctor(std::string first, std::string last, std::string address, int zip, std::string city, int day, int month, int year, int ssn, char gend, double salary, std::string specialty, std::string degree, std::string medSchool, bool certified)
 {
   fName_ = first;
   lName_ = last;
@@ -33,12 +33,70 @@ Doctor::Doctor(std::string first, std::string last, std::string address, int zip
   SSN_ = ssn;
   gender_ = gend;
   salary_ = salary;
-  int docID_ = counter;
-  std::string specialty_ = specialty;
-  std::string degree_ = degree;
-  std::string medSchool_ = medSchool;
-  bool certified_ = certified;
-  Patient* patArray_ = pat;
-  Appointment* appArray_ = app;
+  docID_ = counter;
+  specialty_ = specialty;
+  degree_ = degree;
+  medSchool_ = medSchool;
+  certified_ = certified;
   counter++;
+  nbPatients_ = 0;
+  nbApp_ = 0;
+}
+
+void Doctor::addPatient(Patient* pat)
+{
+  nbPatients_++;
+  Patient** temp = new Patient*[nbPatients_];
+  for (int i = 0; i < (nbPatients_ - 1); i++)
+  {
+    temp[i] = patArray_[i];
+  }
+  temp[(nbPatients_ - 1)] = pat;
+
+  delete[] patArray_;
+
+  patArray_ = new Patient* [nbPatients_];
+  for (int i = 0; i < nbPatients_; i++)
+  {
+    patArray_[i] = temp[i];
+  }
+
+  delete[] temp;
+}
+
+
+void Doctor::addAppointment(int idPatient, int hour, int day, int month, int year, int idNurse)
+{
+    nbApp_++; // increment the total number of vehicles by one since adding a new vehicle
+    Appointment** temp = new Appointment * [nbApp_]; // create a temp array of pointers that will represent the value that we are changing the array to
+    for (int i = 0; i < (nbApp_ - 1); i++) // put in old values into new array up to the last spot
+    {
+        temp[i] = appArray_[i];
+    }
+    Appointment* tempApp;
+    tempApp->hour_ = hour;
+    tempApp->day_ = day;
+    tempApp->month_ = month;
+    tempApp->year_ = year;
+    tempApp->patientID_ = idPatient;
+    tempApp->nurseID_ = idNurse;
+
+    temp[(nbApp_ - 1)] = tempApp; // fill the last spot with the new vehicle being added
+
+    delete[] appArray_; // deallocate the old arrary held within Vehicle** availableVehicles_
+
+    appArray_ = new Appointment * [nbApp_]; // allocate the new memory needed to hold the slightly bigger array
+    for (int i = 0; i < nbApp_; i++) // fill new memory allocated with the correct value stored in temp
+    {
+        appArray_[i] = temp[i];
+    }
+
+    delete[] temp; // deallocate temp
+
+
+}
+
+void deleteAppointment(int idPatient, int hour, int day, int month, int year)
+{
+
 }
--- Doctor.h
+++ Doctor.h
@@ -1,23 +1,28 @@
-#pragma once
 #include "Employee.h"
-#include "Patient.h"
 #include "Appointment.h"
 
 class Doctor : public Employee
 {
 public:
-    Doctor();
-    Doctor(std::string first, std::string last, std::string address, int zip, std::string city, int day, int month, int year, int ssn, char gend, double salary, int docID, std::string specialty, std::string degree, std::string medSchool, bool certified, Patient* pat, Appointment* app);
+  Doctor();
+  Doctor(std::string first, std::string last, std::string address, int zip, std::string city, int day, int month, int year, int ssn, char gend, double salary, std::string specialty, std::string degree, std::string medSchool, bool certified);
+
+void addPatient(Patient* pat);
+
+void addAppointment(int idPatient, int hour, int day, int month, int year, int idNurse);
+
+void deleteAppointment(int idPatient, int hour, int day, int month, int year);
 
 
 private:
-    int docID_;
-    std::string specialty_;
-    std::string degree_;
-    std::string medSchool_;
-    bool certified_;
-    Patient* patArray_;
-    Appointment* appArray_;
-    static int counter;
-
+  int docID_;
+  std::string specialty_;
+  std::string degree_;
+  std::string medSchool_;
+  bool certified_;
+  Patient** patArray_;
+  Appointment** appArray_;
+  static int counter;
+  int nbApp_;
+  int nbPatients_;
 };
